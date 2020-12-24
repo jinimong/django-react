48faf8089d4391bb8ef1aa62bce030bd1c8b558d
jwkim <phoongdeng@gmail.com>
2020-12-25 02:36:06 +0900

    feat(frontend): Add pages and redux

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import PageNotFound from './PageNotFound';
+import Header from './common/Header';
+import ProfilePage from './core/ProfilePage';
+import PrivateRoute from './authentication/PrivateRoute';
+import LoginPage from './authentication/LoginPage';
+import SignUpPage from './authentication/SignUpPage';
 
 function App() {
-  return <h1>Hello from React!</h1>;
+  return (
+    <>
+      <Header />
+      <Switch>
+        <PrivateRoute exact path="/" component={ProfilePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/sign-up" component={SignUpPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
